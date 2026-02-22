e53a83d47603d6f0947389e3b364fcf77f46dfcd
Luke Childs <lukechilds123@gmail.com>
2026-02-22 09:55:14 +0700

    Cache api reponse

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,5 +2,6 @@ import { getData } from '../../lib/data.js';
 
 export default async function handler(req, res) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
 	res.json(await getData());
 }
